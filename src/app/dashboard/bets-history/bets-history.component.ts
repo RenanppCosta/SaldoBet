3f2fc8c0f977772b417b70ad50d303ef16de0fc8
renanppcosta <renanppcosta@gmail.com>
2025-07-30 19:48:03 -0300

    feat: created form add bet in modal

--- src/app/dashboard/bets-history/bets-history.component.ts
+++ src/app/dashboard/bets-history/bets-history.component.ts
@@ -1,17 +1,47 @@
 import { Component, input, signal } from '@angular/core';
 import { BetComponent } from './bet/bet.component';
 import { ButtonComponent } from '../../components/button/button.component';
-import { Bet } from '../models/bet.model';
+import { Bet, PossibleResult } from '../models/bet.model';
 import { ModalComponent } from '../../components/modal/modal.component';
+import { FormsModule } from '@angular/forms';
+import { Game } from '../models/game.model';
 
 @Component({
   selector: 'app-bets-history',
-  imports: [BetComponent, ButtonComponent, ModalComponent],
+  imports: [BetComponent, ButtonComponent, ModalComponent, FormsModule],
   templateUrl: './bets-history.component.html',
   styleUrl: './bets-history.component.css'
 })
 export class BetsHistoryComponent {
   bets = input.required<Bet[]>();
+  games = input.required<Game[]>();
+
+
+  newBetForm = {
+    description: "",
+    result: "",
+    profit:"",
+    date:"",
+    gameName: ""
+  }
+
+  onSubmit(){
+    const selectedGame = this.games().find(game => game.name === this.newBetForm.gameName);
+
+    if (!selectedGame) {
+      return; 
+    }
+
+    const newBet = new Bet(
+      this.newBetForm.description,
+      this.newBetForm.result as PossibleResult,
+      Number(this.newBetForm.profit),
+      this.newBetForm.date,
+      selectedGame
+    )
+
+    console.log(newBet);
+  }
 
   openedModal = signal(false);
 
--- src/app/dashboard/models/bet.model.ts
+++ src/app/dashboard/models/bet.model.ts
@@ -3,14 +3,21 @@ import { Game } from "./game.model";
 
 export class Bet {
     public readonly id = nanoid();
+    public readonly date: Date;
 
     constructor(
         public readonly description: string,
         public readonly result: PossibleResult,
         public readonly profit: number,
-        public readonly date: Date,
+        date: string,
         public readonly game: Game,
-    ){}  
+    ){
+        if (!date.includes('T') || !date.includes(' ')) {
+            date+= "T00:00";
+        }
+        
+        this.date = new Date(date);
+    }  
 }
 
 
