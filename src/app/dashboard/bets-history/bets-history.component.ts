77b1906f5bd1091389a25705ac3ab6ec1ab5c1f5
renanppcosta <renanppcosta@gmail.com>
2025-07-30 00:14:16 -0300

    feat: created logic of modal

--- src/app/dashboard/bets-history/bets-history.component.ts
+++ src/app/dashboard/bets-history/bets-history.component.ts
@@ -1,14 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { BetComponent } from './bet/bet.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { Bet } from '../models/bet.model';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-bets-history',
-  imports: [BetComponent, ButtonComponent],
+  imports: [BetComponent, ButtonComponent, ModalComponent],
   templateUrl: './bets-history.component.html',
   styleUrl: './bets-history.component.css'
 })
 export class BetsHistoryComponent {
   bets = input.required<Bet[]>();
+
+  openedModal = signal(false);
+
+  openModal(){
+    this.openedModal.set(true);
+  }
 }
