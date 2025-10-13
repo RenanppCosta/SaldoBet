9f15f0321675a249889d5c7d70fadfe42c4c3e21
renanppcosta <renanppcosta@gmail.com>
2025-10-12 23:15:27 -0300

    feat: created logic of win rate

--- src/app/dashboard/games/game/game.component.ts
+++ src/app/dashboard/games/game/game.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, input } from '@angular/core';
 import { Game } from '../../models/game.model';
-import { CurrencyPipe, NgClass } from '@angular/common';
+import { CurrencyPipe, NgClass, PercentPipe } from '@angular/common';
+import { PossibleResult } from '../../models/bet.model';
 
 @Component({
   selector: 'app-game',
-  imports: [CurrencyPipe, NgClass],
+  imports: [CurrencyPipe, NgClass, PercentPipe],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
@@ -18,4 +19,37 @@ export class GameComponent {
 
     return "profit-green"
   })
+
+  calcWinRate = (totalBets: number | undefined, numWin: number) =>{
+    if(totalBets != null && totalBets != undefined){
+      return (numWin) / totalBets;
+    }
+
+    return;
+  }
+
+  winRate = computed(() => {
+    const bets = this.game()?.bets;
+    let numWin = 0;
+
+    if (!bets || bets.length === 0) {
+      return 0;
+    }
+    
+    bets.forEach((bet) => {
+      if (bet.result == PossibleResult.WIN) {
+        numWin++; 
+      }
+    });
+
+    const totalBets = bets.length;
+  
+    if (numWin === 0) {
+        return 0;
+    }
+    
+    return this.calcWinRate(totalBets, numWin);
+
+  })
+
 }
